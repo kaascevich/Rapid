2dd25d6fe5a31452b9a8afa3bd3ba0e5dd4d0214
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2023-08-19 20:16:18 -0400

    add square root operator

---
+++ Tests/RapidSpecs/OperatorsSpecs/RootsSpec.swift
@@ -0,0 +1,38 @@
+// RootsSpec.swift
+// Copyright © 2023 Kaleb A. Ascevich
+//
+// This package is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// This package is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this package. If not, see https://www.gnu.org/licenses/.
+
+import Quick
+import Nimble
+@testable import Rapid
+
+final class RootsSpec: QuickSpec {
+    override class func spec() {
+        describe("square roots") {
+            it("returns the square root of a decimal") {
+                expect(√2.2).to(equal(1.483_239_697_419_132_6))
+            }
+            
+            it("returns the square root of an integer") {
+                expect(√64).to(equal(8))
+            }
+            
+            it("fails with negative radicands") {
+                let squareRootOfNegativeFour = √(-4)
+                expect(squareRootOfNegativeFour.isNaN).to(beTrue())
+            }
+        }
+    }
+}
