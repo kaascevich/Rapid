6658523df14ba27b04c7b4f64d4e530f64f07092
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2023-08-22 20:37:18 -0400

    remove docs folder

--- Docs/BuildDocs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-swift package --allow-writing-to-directory "$PWD" \
-    generate-documentation --product Rapid --disable-indexing \
-    --output-path "$PWD/DerivedData"
-
-echo '<script>window.location.href += "/documentation/rapid"</script>' > DerivedData/index.html
