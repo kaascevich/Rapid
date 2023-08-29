8073753b2d834e679c2109eaa833cc5bba24be48
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2023-08-29 10:58:21 -0400

    add documentation for PartialRange == operators

--- Sources/Rapid/Equatable/EquatableRange.swift
+++ Sources/Rapid/Equatable/EquatableRange.swift
@@ -15,18 +15,66 @@
 // with this package. If not, see https://www.gnu.org/licenses/.
 
 extension PartialRangeFrom: Equatable {
+    /// Returns a Boolean value indicating whether two partial ranges are
+    /// equal.
+    ///
+    /// Two partial ranges are equal when they have the same lower bounds.
+    ///
+    ///     let x = 5...
+    ///     print(x == 5...)
+    ///     // Prints "true"
+    ///     print(x == 10...)
+    ///     // Prints "false"
+    ///
+    /// - Parameters:
+    ///   - lhs: A range to compare.
+    ///   - rhs: Another range to compare.
+    ///
+    /// - Returns: Whether the two ranges are equal.
     public static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.lowerBound == rhs.lowerBound
     }
 }
 
 extension PartialRangeThrough: Equatable {
+    /// Returns a Boolean value indicating whether two partial ranges are
+    /// equal.
+    ///
+    /// Two partial ranges are equal when they have the same upper bounds.
+    ///
+    ///     let x = ...15
+    ///     print(x == ...15)
+    ///     // Prints "true"
+    ///     print(x == ...20)
+    ///     // Prints "false"
+    ///
+    /// - Parameters:
+    ///   - lhs: A range to compare.
+    ///   - rhs: Another range to compare.
+    ///
+    /// - Returns: Whether the two ranges are equal.
     public static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.upperBound == rhs.upperBound
     }
 }
 
 extension PartialRangeUpTo: Equatable {
+    /// Returns a Boolean value indicating whether two partial ranges are
+    /// equal.
+    ///
+    /// Two partial ranges are equal when they have the same upper bounds.
+    ///
+    ///     let x = ..<15
+    ///     print(x == ..<15)
+    ///     // Prints "true"
+    ///     print(x == ..<20)
+    ///     // Prints "false"
+    ///
+    /// - Parameters:
+    ///   - lhs: A range to compare.
+    ///   - rhs: Another range to compare.
+    ///
+    /// - Returns: Whether the two ranges are equal.
     public static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.upperBound == rhs.upperBound
     }
