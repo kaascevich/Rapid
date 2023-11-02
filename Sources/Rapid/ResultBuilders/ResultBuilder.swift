5dcfd526e9eb45c58d9a5d6172a52b96d08e9b28
Kaleb Ascevich <swimmer-flowing-0s@icloud.com>
2023-11-02 14:34:10 -0400

    alter code examples

--- Sources/Rapid/ResultBuilders/ResultBuilder.swift
+++ Sources/Rapid/ResultBuilders/ResultBuilder.swift
@@ -110,7 +110,8 @@ public extension ResultBuilder {
     ///
     /// ```swift
     /// let string = String {
-    ///     "Hello, World!"
+    ///     "Hello, "
+    ///     "World!"
     /// }
     /// // string == "Hello, World!"
     /// ```
@@ -127,9 +128,7 @@ public extension ResultBuilder {
     ///
     /// ```swift
     /// let string = String {
-    ///     for i in 1...3 {
-    ///         §i
-    ///     }
+    ///     for i in 1...3 { §i }
     /// }
     /// // string == "123"
     /// ```
