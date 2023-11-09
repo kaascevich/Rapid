6879e3e13d7c7c3b384384c94c3b3c51908c384f
Kaleb Ascevich <129605200+kaascevich@users.noreply.github.com>
2023-11-09 18:15:50 -0500

    move where clauses to extension declaration

--- Sources/Rapid/Sequences/Sum.swift
+++ Sources/Rapid/Sequences/Sum.swift
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License along
 // with this package. If not, see https://www.gnu.org/licenses/.
 
-public extension Sequence {
+public extension Sequence where Element: Numeric {
     /// Returns the result of adding the elements of the sequence.
     ///
     /// Use the `sum()` method to produce a sum from the elements of an
@@ -25,7 +25,7 @@ public extension Sequence {
     ///
     /// - Returns: The sum of the sequence's elements. If the sequence has no
     ///   elements, the result is `0`.
-    func sum() -> Element where Element: Numeric {
+    func sum() -> Element {
         reduce(.zero, +)
     }
     
@@ -39,7 +39,7 @@ public extension Sequence {
     ///
     /// - Returns: The product of the sequence's elements. If the sequence
     ///   has no elements, the result is `1`.
-    func product() -> Element where Element: Numeric {
+    func product() -> Element {
         reduce(1, *)
     }
 }
